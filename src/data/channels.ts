ae923f1e851c6de072f5214579b150994608337e
jamesgpt143-arch <jamesgpt143@gmail.com>
2025-12-03 16:16:00 +0800

    Update channels.ts

--- src/data/channels.ts
+++ src/data/channels.ts
@@ -25,7 +25,7 @@ export const channels: Channel[] = [
   type: 'mpd',
   logo: 'https://i.imgur.com/pRwyOMP.png'
 }, {
-    name: '&Flix HD',
+    name: '&Flix',
   manifestUri: 'https://edge3-moblive.yuppcdn.net/drm/smil:nflixdrm.smil/chunklist_b996000.m3u8',
   type: 'hls',
   logo: 'https://ts1.mm.bing.net/th?id=OIP.FlPpt1NoTPALgSMM8CwFgAHaHa&pid=15.1'
